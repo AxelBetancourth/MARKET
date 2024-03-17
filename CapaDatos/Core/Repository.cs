d9da2f55975a2ef04d1b321c82664ae297f03eef
Axel <abetancourth2004@gmail.com>
2024-03-16 22:15:23 -0600

    Creacion del Proyecto

---
+++ CapaDatos/Core/Repository.cs
@@ -0,0 +1,24 @@
+﻿using CapaDatos.BaseDatos;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos.Core
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly MarketContext dbcontext;
+
+        public Repository()
+        {
+            dbcontext = new MarketContext();
+        }
+        public void Agregar(T entidad)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
