2dd49ac33739d16e8ffd74b4c06e3426c48ea953
ClaudioTejeda <claudiotejeda945@gmail.com>
2024-03-18 12:12:17 -0600

    Adicion de tabla productos

---
+++ CapaDatos/BaseDatos/Modelos/MProductos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos.BaseDatos.Modelos
+{
+    public class MProductos
+    {
+        [Key]
+        public int ProductoId { get; set; }
+        [Required]
+        public int Categoriaid { get; set; }
+        public MCategorias MCategotias { get; set; }
+        public int UnidadMedidaId { get; set; }
+        public MUnidadMedidas MUnidadMedidas { get; set; }
+        [Required]
+        public DateTime FechaCreacion { get; set; }
+        public bool Estado { get; set; }
+        [Required]
+        public decimal PrecioCompra { get; set; }
+    }
+}
