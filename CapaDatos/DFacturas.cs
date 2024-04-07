bbf525f6a1056900fc0cf57f678a406bb1e25bcb
Axel <abetancourth2004@gmail.com>
2024-04-07 09:21:09 -0600

    Fix Facturas

---
+++ CapaDatos/DFacturas.cs
@@ -0,0 +1,76 @@
+﻿using CapaDatos.BaseDatos.Modelos;
+using CapaDatos.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class DFacturas
+    {
+        UnitOfWork _unitOfWork;
+
+        public DFacturas()
+        {
+            _unitOfWork = new UnitOfWork();
+        }
+
+        public int FacturaId { get; set; }
+        public int ClienteID { get; set; }
+        public int PedidoID { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public DateTime FechaFactura { get; set; }
+        public bool Estado { get; set; }
+        public decimal Total { get; set; }
+        public decimal SubTotal { get; set; }
+        public decimal Descuento { get; set; }
+
+
+        public List<MFacturas> TodosLasFacturas()
+        {
+            return _unitOfWork.Repository<MFacturas>().Consulta().Include(c => c.MClientes)
+                                         .Include(c => c.MPedidos)
+                                         .ToList();
+        }
+        public int Guardar(MFacturas facturas)
+        {
+            if (facturas.FacturaId == 0)
+            {
+                _unitOfWork.Repository<MFacturas>().Agregar(facturas);
+                return _unitOfWork.Guardar();
+            }
+            else
+            {
+                var facturaInDb = _unitOfWork.Repository<MFacturas>().Consulta().FirstOrDefault(c => c.FacturaId == facturas.FacturaId);
+                if (facturaInDb != null)
+                {
+                    facturaInDb.ClienteID = facturas.ClienteID;
+                    facturaInDb.PedidoID = facturas.PedidoID;
+                    facturas.FechaFactura = facturaInDb.FechaFactura;
+                    facturaInDb.Estado = facturas.Estado;
+                    facturaInDb.Total = facturas.Total;
+                    facturaInDb.SubTotal = facturas.SubTotal;
+                    facturaInDb.Descuento = facturas.Descuento;
+                    _unitOfWork.Repository<MFacturas>().Editar(facturaInDb);
+                    return _unitOfWork.Guardar();
+                }
+
+                return 0;
+            }
+        }
+        public int Eliminar(int FacturaId)
+        {
+            var facturaInDb = _unitOfWork.Repository<MFacturas>().Consulta().FirstOrDefault(c => c.FacturaId == FacturaId);
+            if (facturaInDb != null)
+            {
+                _unitOfWork.Repository<MFacturas>().Eliminar(facturaInDb);
+                return _unitOfWork.Guardar();
+            }
+            return 0;
+        }
+    }
+}
---
+++ MARKET/PFacturas.cs
@@ -0,0 +1,175 @@
+﻿using CapaDatos;
+using CapaDatos.BaseDatos.Modelos;
+using CapaNegocio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MARKET
+{
+    public partial class PFacturas : Form
+    {
+        private NFacturas nFacturas;
+        private NClientes nClientes;
+        private NPedidos nPedidos;
+
+        public PFacturas()
+        {
+            InitializeComponent();
+            nFacturas = new NFacturas();
+            nClientes = new NClientes();
+            nPedidos = new NPedidos();
+            CargarDatos();
+            CargarCombos();
+        }
+
+        private void PFacturas_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        void CargarDatos()
+        {
+            var datos = nFacturas.obtenerFacturasGrid();
+            dgFacturas.DataSource = datos;
+        }
+
+        private void CargarCombos()
+        {
+            cbxclientes.DataSource = nClientes.CargaCombo();
+            cbxclientes.ValueMember = "Valor";
+            cbxclientes.DisplayMember = "Descripcion";
+
+            cbxpedidos.DataSource = nPedidos.CargaCombo();
+            cbxpedidos.ValueMember = "Valor";
+            cbxpedidos.DisplayMember = "Descripcion";
+        }
+
+        void LimpiarDatos()
+        {
+            txtFacturaId.Text = "";
+            cbxclientes.SelectedValue = "";
+            cbxpedidos.Text = "";
+            fechafactura.Value = DateTime.Now;
+            cbEstado.Checked = false;
+            txttotal.Text = "";
+            txtsubtotal.Text = "";
+            txtdescuento.Text = "";
+            errorProvider1.Clear();
+        }
+
+        private bool ValidarDatos()
+        {
+            var FormularioValido = true;
+            if (string.IsNullOrEmpty(cbxclientes.Text.ToString()) || string.IsNullOrWhiteSpace(cbxclientes.Text.ToString()))
+            {
+                FormularioValido = false;
+                errorProvider1.SetError(cbxclientes, "Debe ingrear un Cliente");
+                return FormularioValido;
+            }
+            if (string.IsNullOrEmpty(cbxpedidos.Text.ToString()) || string.IsNullOrWhiteSpace(cbxpedidos.Text.ToString()))
+            {
+                FormularioValido = false;
+                errorProvider1.SetError(cbxpedidos, "Debe de ingresar un pedido");
+                return FormularioValido;
+            }
+            if (string.IsNullOrEmpty(txttotal.Text.ToString()) || string.IsNullOrWhiteSpace(txttotal.Text.ToString()))
+            {
+                FormularioValido = false;
+                errorProvider1.SetError(txttotal, "Debe ingresar un monto total");
+                return FormularioValido;
+            }
+            if (string.IsNullOrEmpty(txtsubtotal.Text.ToString()) || string.IsNullOrWhiteSpace(txtsubtotal.Text.ToString()))
+            {
+                FormularioValido = false;
+                errorProvider1.SetError(txtsubtotal, "Debe ingresar un monto subtotal");
+                return FormularioValido;
+            }
+            if (string.IsNullOrEmpty(txtdescuento.Text.ToString()) || string.IsNullOrWhiteSpace(txtdescuento.Text.ToString()))
+            {
+                FormularioValido = false;
+                errorProvider1.SetError(txtdescuento, "Debe ingresar un descuento");
+                return FormularioValido;
+            }
+            return FormularioValido;
+        }
+
+        private void cbEstado_CheckedChanged(object sender, EventArgs e)
+        {
+            dgFacturas.DataSource = nFacturas.obtenerFacturasActivasGrid();
+            if (cbActivos.Checked == false)
+            {
+                CargarDatos();
+            }
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (ValidarDatos())
+            {
+                MFacturas facturas = new MFacturas()
+
+                {
+                    ClienteID = int.Parse(cbxclientes.SelectedValue.ToString()),
+                    PedidoID = int.Parse(cbxpedidos.SelectedValue.ToString()),
+                    FechaFactura = fechafactura.Value,
+                    Estado = cbEstado.Checked,
+                    Total = decimal.Parse(txttotal.Text.ToString()),
+                    SubTotal = decimal.Parse(txtsubtotal.Text.ToString()),
+                    Descuento = decimal.Parse(txtdescuento.Text.ToString()),
+
+                };
+                if (!string.IsNullOrEmpty(txtFacturaId.Text) || !string.IsNullOrWhiteSpace(txtFacturaId.Text))
+                {
+                    if (int.TryParse(txtFacturaId.Text.ToString(), out int facturaId) && facturaId != 0)
+                    {
+                        facturas.FacturaId = facturaId;
+                    }
+                }
+                nFacturas.AgregarFacturas(facturas);
+                LimpiarDatos();
+                CargarDatos();
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            var FacturaId = txtFacturaId.Text.ToString();
+            if (string.IsNullOrEmpty(FacturaId) || string.IsNullOrWhiteSpace(FacturaId))
+            {
+                return;
+            }
+            nFacturas.Eliminar(int.Parse(FacturaId));
+            CargarDatos();
+            LimpiarDatos();
+        }
+
+        private void dgFacturas_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.RowIndex < dgFacturas.Rows.Count)
+            {
+                DataGridViewRow row = dgFacturas.Rows[e.RowIndex];
+                txtFacturaId.Text = row.Cells["FacturaID"].Value.ToString();
+                var clientes = dgFacturas.CurrentRow.Cells["ClienteNombreCompleto"].Value.ToString();
+                cbxclientes.SelectedIndex = cbxclientes.FindStringExact(clientes);
+                var pedidos = dgFacturas.CurrentRow.Cells["PedidoInfo"].Value.ToString();
+                cbxpedidos.SelectedIndex = cbxpedidos.FindStringExact(pedidos);
+                var fechaFactura = row.Cells["FechaFactura"].Value;
+                if (fechaFactura != null)
+                {
+                    fechafactura.Value = (DateTime)fechaFactura;
+                }
+                cbEstado.Checked = bool.Parse(dgFacturas.CurrentRow.Cells["Estado"].Value.ToString());
+                txttotal.Text = row.Cells["Total"].Value.ToString();
+                txtsubtotal.Text = row.Cells["SubTotal"].Value.ToString();
+                txtdescuento.Text = row.Cells["Descuento"].Value.ToString();
+            }
+        }
+    }
+}
